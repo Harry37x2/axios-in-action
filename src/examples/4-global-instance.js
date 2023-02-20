e37fd6282278e74de192ed801a442a3f91c5667f
Harry37x2 <bartek3737@o2.pl>
2023-02-20 19:12:30 +0100

    initial commit

---
+++ src/examples/4-global-instance.js
@@ -0,0 +1,24 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+
+const productsUrl = 'https://course-api.com/react-store-products';
+const randomUserUrl = 'https://randomuser.me/api';
+
+const GlobalInstance = () => {
+  const fetchData = async () => {
+    try {
+      const resp = await axios(productsUrl);
+      const resp1 = await axios(randomUserUrl);
+      console.log(resp)
+    } catch (error) {
+      
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return <h2 className='text-center'>global instance</h2>;
+};
+export default GlobalInstance;
